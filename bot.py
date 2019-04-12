4b6e209a30a65a76733a40e53506a60a7a7a63ae
BlueBirdYT <46397900+BlueBirdYT@users.noreply.github.com>
2019-04-12 20:20:27 +0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -1352,7 +1352,7 @@ async def on_message(message):
         msg2 = await client.send_message(message.channel, msg)
     if message.server.id == '560515451914813440':
         return
-        if message.content.lower().startswith('a!rank'):
+    if message.content.lower().startswith('a!rank'):
         r, g, b = tuple(int(x * 255) for x in colorsys.hsv_to_rgb(random.random(), 1, 1))
         level=int(get_xp(message.author.id)/100)
         msgs=int(get_xp(message.author.id)/2)
